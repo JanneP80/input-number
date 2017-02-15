86345b61de5e780d1084369a3325a550324ca73f
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-15 10:08:18 +0200

    fixes

--- input-number/Program.cs
+++ input-number/Program.cs
@@ -12,14 +12,36 @@ namespace input_number
 
         static void Main(string[] args)
         {
-            // member variables
-            // int userinputnumber;
-
+            
             // checs if number is odd or even or zero
+            int inputnumber=0;
+            string message = string.Empty;
+            Console.Write("This program checks is typed number is odd/even or zero. \nType number: ");
+            bool result = int.TryParse(Console.ReadLine(), out inputnumber);
+            if (!result) // incorrect input
+            {
+                message = "Input is NOT a number.";
+            }
+            else if (inputnumber == 0) // is zero
+            {
+                message = string.Format("You typed number {0}, it is zero.", inputnumber);
+            }
+            else if (inputnumber % 2 == 0) //divided, true, is even
+            {
+                message = string.Format("You typed number {0} and it is even.", inputnumber);
+            }
+            else // else odd
+            {
+                message = string.Format("You typed number {0} and it is odd.", inputnumber);
+            }
+            Console.WriteLine(message);
+            Console.ReadKey();
 
+            // poikki tästä
+            /*
             Console.Write("Type number: ");
-            int userinputnumber = Convert.ToInt32(Console.ReadLine(), out number);
-			if (!result) 
+            bool userinputnumber = Convert.ToInt32(Console.ReadLine(), out number);
+			if (!userinputnumber) 
 			{
 				Console.WriteLine("Not a number {0}, try again.", userinputnumber);
 			}
@@ -48,9 +70,10 @@ namespace input_number
             if (userinputnumber == %2) // 
             {
                 Console.WriteLine("Number {0} is even.", userinputnumber);
-            }*/
+            }*/ 
+            /*
             else return;
-            Console.ReadKey();
+            Console.ReadKey(); */
         }
     }
 }

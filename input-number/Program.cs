b7bc130b1ffb93164fa611db51df6d5434faf577
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-15 09:30:27 +0200

    Added new files

---
+++ input-number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input_number
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            // member variables
+            // int userinputnumber;
+
+            // checs if number is odd or even or zero
+
+            Console.Write("Type number: ");
+            int userinputnumber = Convert.ToInt32(Console.ReadLine());
+
+            if (userinputnumber == 0) // is zero
+            {
+                Console.WriteLine("Number {0} is zero.", userinputnumber);
+            }
+            else
+            if (userinputnumber != %2) // if not 
+            {
+                Console.WriteLine("Number {0} is odd.", userinputnumber);
+            }
+            else
+            if (userinputnumber == %2) // 
+            {
+                Console.WriteLine("Number {0} is even.", userinputnumber);
+            }
+            else return;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ input-number/README.md
@@ -0,0 +1,2 @@
+Added input-number
+checs if number is odd or even or zero
